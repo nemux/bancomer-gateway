6826a6df77699a614046592408b0f6ebe592465b
Josue Guillermo Araujo Mayorga <nemux@NMX-LAPTOP.NMX>
2017-08-23 21:58:47 -0500

    Version 1.0 Fake Bancomer Gateway

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Bancomer Gateway' });
+});
+
+router.post('/pay', function(req, res, next){
+    var num = Math.floor(Math.random() * 5) + 1;
+
+    if(num > 1){
+     res.json({result: 'accepted'});
+    } else {
+      res.json({result: 'rejected'});
+    }
+});
+
+module.exports = router;
